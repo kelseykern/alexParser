7818b483fb9bd00f3705b5f6b79c8a0846412c0a
kelseykern <kelseykern@users.noreply.github.com>
2023-11-28 14:00:28 -0600

    update lgoic

--- Alex/alex.py
+++ Alex/alex.py
@@ -1,15 +1,32 @@
-FILENAME = 'test_file.txt'
+#SOLID and RESTFUL and (OOP or “object oriented”)
+sr = "no"
 
-#def parse_file_and_check_word(file_path, user_input):
-#    with open(file_path) as data:
-#        while line := data.readline():
-#            q, *d = map(str.strip, line.split('-'))
-#            if d and q == user_input:
-#                return d[0]
+#Azure or AWS or OOP or “object oriented” or “dependency injection”
+azs = "no"
 
-#print(parse_file_and_check_word(FILENAME, '.net'))
-#print(parse_file_and_check_word(FILENAME, 'C++'))
+#Debug or troubleshoot or diagnose or improve
+debug_exp = "no"
 
+#“.net” and “c#” and SQL and Azure
+net_c = "no"
+
+#Leadership or mentorship
+leader= "no"
+
+#“visual studio” or “visual basic” or GIT
+vs = "no"
+
+#Agile or scrum or “CI/CD” 
+agile = "no"
+
+#Design* or concept*
+design = "no"
+
+#Github or Gitlab or bitbucket or Jira or Asana or Trello
+gith = "no"
+
+#Manage* near project
+manag = "no"
 
 with open('test_file.txt','r') as file:
   
@@ -18,8 +35,58 @@ with open('test_file.txt','r') as file:
   
         # reading each word        
         for word in line.split():
-  
-            # displaying the words           
+            
+            # if SOLID and RESTFUL and (OOP or “object oriented”)
+            #    sr = "yes"
+
+            # if Azure or AWS or OOP or “object oriented” or “dependency injection”
+            #   azs = "yes"
+
+            # if Debug or troubleshoot or diagnose or improve
+            #   debug_exp = "yes"
+
+            # if “.net” and “c#” and SQL and Azure          
             if word in ('C++', 'c++',  '.net', '.NET'):
-                print(word)
+                net_c = "yes"
+
+            # if Leadership or mentorship
+            #   leader= "yes"
+
+            # if “visual studio” or “visual basic” or GIT
+            #   vs = "yes"
+
+            # if Agile or scrum or “CI/CD” 
+            #    agile = "yes"
+
+            # if Design* or concept*
+            #   design = "yes"
+
+            # if Github or Gitlab or bitbucket or Jira or Asana or Trello
+            #   gith = "yes"
+
+            # if Manage* near project
+            #   manag = "yes"
+
+print (
+"SOLID and RESTFUL = " + sr
+
++ "\nAzure or AWS = " + azs
+
++ "\nDebug or troubleshoot or diagnose or improve = " + debug_exp
+
++ "\n.net and c# and SQL and Azure = " + net_c
+
++ "\nLeadership or mentorship = " + leader
+
++ "\nvisual studio or visual basic or GIT = " + vs
+
++ "\nAgile or scrum or CI/CD = " + agile
+
++ "\nDesign or concept = " + design
+
++ "\nGithub or Gitlab or bitbucket or Jira or Asana or Trello = " + gith
+
++ "\nManage* near project = " + manag
+)
+
            
